f81783beeaebca4ed22ae9daa6d3ab44d72e9335
manrajgarg <50338769+manrajgarg@users.noreply.github.com>
2022-05-24 21:07:37 -0400

    test vercel

--- components/Banner.js
+++ components/Banner.js
@@ -13,7 +13,7 @@ function Banner() {
       <div className="absolute top-1/2 w-full text-center">
         <p className="text-sm sm:text-lg">Not sure where to go? Perfect.</p>
         <button className="my-3 rounded-full bg-white px-10 py-4 font-bold text-purple-500 shadow-md transition duration-150 hover:shadow-xl active:scale-90">
-          I'm flexible
+          I&apos;m flexible
         </button>
       </div>
     </div>
